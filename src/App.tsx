7d78332b96bd6e9f8ba1f95ad3b237b593826443
paulosvdd05 <paulosvdd05@gmail.com>
2023-04-03 08:32:25 -0300

    criando texto central

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SafeAreaView, Text} from 'react-native'
+import TextoCentral from './components/TextoCentral'
+
+
+export default props => {
+    return (
+        <SafeAreaView style={{flex:1}}>
+            <TextoCentral>App</TextoCentral>
+        </SafeAreaView>
+    )
+}
---
+++ src/components/TextoCentral.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+
+export default props => {
+    return (
+        <View style={{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        }}>
+            <Text style={{fontSize:50}}>
+                {props.children}
+            </Text>
+        </View>
+    )
+}
